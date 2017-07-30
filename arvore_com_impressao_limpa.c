bc11ae68d035b07b1e69478f6a10842433b4ff8f
Unknown <raoneargolo@gmail.com>
2017-07-30 00:53:53 -0300

    Arrumações na identação

--- arvore_com_impressao_limpa.c
+++ arvore_com_impressao_limpa.c
@@ -25,69 +25,77 @@ int main( int argc, char **argv )
     int item; // variável para armazenar os valores randômicos criados
     nodo_arvore_ponteiro ponteiro_raiz = NULL; // inicializando a árvore como nula
 
-    srand( time( NULL ) );
-    puts( "Os numeros a serem inseridos na arvore sao:" );
+    srand(time(NULL));
+    puts("Os numeros a serem inseridos na arvore sao:");
 
     // inserindo valores randômicos entre 0 e 14 na árvore
-    for ( i = 1; i <= 20; ++i )
+    for (i = 1; i <= 20; ++i)
     {
         item = rand() % 100;
-        printf( "%3d", item );
-        inserir_na_arvore( &ponteiro_raiz, item); //insere valores aleatorios no nodo
+        printf("%3d", item);
+        inserir_na_arvore(&ponteiro_raiz, item); //insere valores aleatorios no nodo
     } // fim do laço for
     
     // percorrendo percurso em ordem
-    puts( "\n\nO percuro em-ordem:" );
-    percurso_em_ordem( ponteiro_raiz );
+    puts("\n\nO percuro em-ordem:");
+    percurso_em_ordem(ponteiro_raiz);
 
     printf("\n");
 
-    print2D(ponteiro_raiz);
+    impressao_formato_arvore(ponteiro_raiz);
 
     exit(0);
 } // fim da função principal
 
 // inserindo nodo na árvore
-void inserir_na_arvore( nodo_arvore_ponteiro *ponteiro_arvore, int valor )
+void inserir_na_arvore(nodo_arvore_ponteiro *ponteiro_arvore, int valor)
 {
     // se a árvore estiver vazia
-    if ( *ponteiro_arvore == NULL ) {
-        *ponteiro_arvore = malloc( sizeof( nodo_arvore ) );
+    if (*ponteiro_arvore == NULL)
+    {
+        *ponteiro_arvore = malloc(sizeof(nodo_arvore));
 
         // se um espaço de memória foi alocado, então o nodo recebe o dado
-        if ( *ponteiro_arvore != NULL ) {
-            ( *ponteiro_arvore )->dado = valor;
-            ( *ponteiro_arvore )->ponteiro_esquerda = NULL;
-            ( *ponteiro_arvore )->ponteiro_direita = NULL;
+        if (*ponteiro_arvore != NULL)
+        {
+            (*ponteiro_arvore)->dado = valor;
+            (*ponteiro_arvore)->ponteiro_esquerda = NULL;
+            (*ponteiro_arvore)->ponteiro_direita = NULL;
         } // fim do condicional if
-        else {
+        else
+        {
             printf( "%d nao inserido. Sem memoria disponivel.\n", valor );
         } // fim do condicional else
     } // fim do condional if
-    else { // a árvore não está vazia
+    else // a árvore não está vazia
+    {
         // valor a ser inserido na árvore é menor que o valor presente no nodo atual
-        if ( valor < ( *ponteiro_arvore )->dado ) {
-            inserir_na_arvore( &( ( *ponteiro_arvore )->ponteiro_esquerda ), valor );
+        if (valor < (*ponteiro_arvore)->dado)
+        {
+            inserir_na_arvore(&((*ponteiro_arvore)->ponteiro_esquerda), valor );
         } // fim do condicional if
         
         // valor a ser inserido na árvore é maior que o valor presente no nodo atual
-        else if ( valor > ( *ponteiro_arvore )->dado ) {
-            inserir_na_arvore( &( ( *ponteiro_arvore )->ponteiro_direita ), valor );
+        else if (valor > (*ponteiro_arvore)->dado)
+        {
+            inserir_na_arvore(&((*ponteiro_arvore)->ponteiro_direita), valor);
         } // fim do condicional else if
-        else { // ignorar valor dentro nó duplicado
-            printf( "%s", " (valor duplicado)" );
+        else// ignorar valor dentro nó duplicado
+        { 
+            //printf("%s", " (valor duplicado)");
         } // fim do condicional else
     } // fim do condicional else
 } // fim da função inserir_na_arvore
 
 // inicia percurso em ordem da árvore
-void percurso_em_ordem( nodo_arvore_ponteiro ponteiro_arvore )
+void percurso_em_ordem(nodo_arvore_ponteiro ponteiro_arvore)
 {
     // se a árvore não está vazia, então percorre a mesma
-    if ( ponteiro_arvore != NULL ) {
-        percurso_em_ordem( ponteiro_arvore->ponteiro_esquerda );
-        printf( "%3d", ponteiro_arvore->dado );
-        percurso_em_ordem( ponteiro_arvore->ponteiro_direita );
+    if (ponteiro_arvore != NULL)
+    {
+        percurso_em_ordem(ponteiro_arvore->ponteiro_esquerda);
+        printf("%3d", ponteiro_arvore->dado);
+        percurso_em_ordem(ponteiro_arvore->ponteiro_direita);
     } // fim do condicional if
 }// fim da função percurso_em_ordem
 
@@ -109,7 +117,10 @@ void impressao_formato_arvore(nodo_arvore_ponteiro ponteiro_raiz, int espaco)
     printf("\n");
     int i;
     for (i = COUNT; i < espaco; i++)
+    {
         printf(" ");
+    }
+    
     printf("%d\n", ponteiro_raiz->dado);
  
     // Processa nós a esquerda
